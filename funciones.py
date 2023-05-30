fa8ad94adb640d2f3459b10b49acd30540f727cd
Jesús Zelaya <88550504+jesuszelayac@users.noreply.github.com>
2023-05-30 14:03:45 -0500

    Create funciones.py
    Anotaciones iniciales de estructura

---
+++ funciones.py
@@ -0,0 +1,4 @@
+#Estructura Archivos
+#1) Primero solo colocaremos los codigos usados mayormente
+#2) Segundo les dare forma de funcion y documento como debe ser su input
+#Nota: Asegurate de colocar la version de las librerias usadas
