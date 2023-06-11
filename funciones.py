3a9f108a517bb3035b8635cc4d16695534101661
Jesús Zelaya <88550504+jesuszelayac@users.noreply.github.com>
2023-06-10 19:18:56 -0500

    Delete funciones.py

--- funciones.py
+++
@@ -1,4 +0,0 @@
-#Estructura Archivos
-#1) Primero solo colocaremos los codigos usados mayormente
-#2) Segundo les dare forma de funcion y documento como debe ser su input
-#Nota: Asegurate de colocar la version de las librerias usadas
